a12b76d96d0eaefb989d6598b31b8d0a636668d8
retsamknaps <retsamknaps@hmamail.com>
2015-10-31 20:00:54 -0500

    0.2.6 release.

--- src/aktie/gui/Wrapper.java
+++ src/aktie/gui/Wrapper.java
@@ -32,7 +32,7 @@ public class Wrapper
     //the upgrade file added to the network by the developer account.
     //This keeps new installs from downloading the same version as
     //an upgrade
-    public static long RELEASETIME = ( 1435501588L * 1000L ) + 3600000;
+    public static long RELEASETIME = ( 1446338069L * 1000L ) + 3600000;
 
     public static String RUNDIR = "aktie_run_dir";
     public static String LIBDIR = RUNDIR + File.separator + "lib";
