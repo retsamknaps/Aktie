616ab153799b86792729a929a3877d14ad058d1f
retsamknaps <retsamknaps@hmamail.com>
2016-05-26 07:26:53 -0500

    0.4.17

--- src/aktie/gui/Wrapper.java
+++ src/aktie/gui/Wrapper.java
@@ -25,9 +25,9 @@ public class Wrapper
     public static String VERSION_0115 = "version 0.1.15";
     public static String VERSION_0403 = "version 0.4.3";
     public static String VERSION_0405 = "version 0.4.5";
-    public static String VERSION_0416 = "version 0.4.16";
+    public static String VERSION_0417 = "version 0.4.17";
 
-    public static String VERSION = VERSION_0416;
+    public static String VERSION = VERSION_0417;
 
     public static String VERSION_FILE = "version.txt";
 
@@ -36,7 +36,7 @@ public class Wrapper
     //the upgrade file added to the network by the developer account.
     //This keeps new installs from downloading the same version as
     //an upgrade
-    public static long RELEASETIME = ( 1463782256L * 1000L ) + 3600000L;
+    public static long RELEASETIME = ( 1464264565L * 1000L ) + 3600000L;
 
     public static String RUNDIR = "aktie_run_dir";
     public static String LIBDIR = RUNDIR + File.separator + "lib";
